e66cebee2da621a89ce152d76a2f6397b1164ace
sahrios <sahriosrodrigues@gmail.com>
2022-10-21 22:48:08 -0300

    enviando a to do list

---
+++ README.md
@@ -0,0 +1,11 @@
+# To-do List
+
+Projeto construído no módulo de JavaScript do Ciclo Formativo Preta Lab.
+
+[🔗Clique aqui para acessar] 
+## 🛠️👩🏾‍💻 Tecnologias
+
+- HTML
+- CSS
+- JavaScript
+- Git e Github
---
+++ index.js
@@ -0,0 +1,17 @@
+let id= 0
+
+const tarefa = (id,novaTarefa) =>`<div id = '${id}'>
+<p>${novaTarefa}</p>
+<input type ="checkbox" />
+</div>`
+
+function adicionarTarefa(){
+    id++
+    const novaTarefa = document.getElementById('nome-tarefa').value
+   document.querySelector ('#lista-tarefas').innerHTML += tarefa (id,novaTarefa)
+}
+
+
+function excluirTarefa() {
+    document.getElementById('lista-tarefas').lastChild.remove();
+}
